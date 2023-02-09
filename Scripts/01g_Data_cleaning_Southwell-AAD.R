da42a32d7af0a63b04401f142a5edcb3988b8748
lidefi87 <lidefi87@hotmail.com>
2023-02-09 17:26:09 +0800

    Adding cleaning scripts for SCAR APIS and Colin Southwell's datasets

---
+++ Scripts/01g_Data_cleaning_Southwell-AAD.R
@@ -0,0 +1,10 @@
+###############################################################################
+# Cleaning crabeater seal data obtained from Colin Southwell - AAD
+# Author: Denisse Fierro Arcos
+# Date: 2023-02-07
+# 
+# Crabeater seal data downloaded from Colin Southwell - AAD
+
+# Loading libraries -------------------------------------------------------
+library(tidyverse)
+library(CoordinateCleaner)
